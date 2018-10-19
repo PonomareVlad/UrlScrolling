aa14f8da4483d66ef5b5ee21de2cd28470d8d184
PonomareVlad <sys.system@mail.ru>
2018-10-19 20:43:09 +0500

    Scroll test

--- urlScroll.js
+++ urlScroll.js
@@ -1,7 +1,22 @@
 window.addEventListener('popstate', router);
 window.addEventListener('load', router);
+document.body.addEventListener('scroll', scrollHandler);
+
+
+function scrollHandler() {
+    document.querySelectorAll('section').forEach(function (el) {
+        if (isScrolledIntoView(el)) history.replaceState(false, el.innerText, el.id);
+    })
+}
 
 function router() {
     let targetSection = document.getElementById(location.pathname);
     if (targetSection) targetSection.scrollIntoView();
+}
+
+function isScrolledIntoView(el) {
+    let rect = el.getBoundingClientRect();
+    let elemTop = rect.top;
+    let elemBottom = rect.bottom;
+    return (elemTop >= 0) && (elemBottom <= window.innerHeight);
 }
